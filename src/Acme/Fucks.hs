b8d9384634986b684d1eddacc823604bcce9312b
Akii <zedd@akii.de>
2017-01-27 09:23:21 +0100

    Don't expose Fucks constructor and implement Monad differently
    FucksWith #6
    FucksWith #7

--- src/Acme/Fucks.hs
+++ src/Acme/Fucks.hs
@@ -1,12 +1,16 @@
 module Acme.Fucks
-  ( Fucks(..)
+  ( Fucks
+  , Amount
+  , giveFucks
   ) where
 
 import Control.Monad
 
+type Amount = Int
+
 -- | Tells us how many fucks were given that day.
 -- We make it a phantom type (parametrized over `f`) because we would like to pretend we're able to give a fuck about anything.
-newtype Fucks f = Fucks Int
+newtype Fucks f = Fucks Amount
 
 -- | The essence of how many fucks should be given.
 -- It first, we don't give a fuck. If at any point, somehow a fuck is given, we make sure to not give a fuck again.
@@ -18,7 +22,7 @@ instance Monoid (Fucks f) where
 --
 -- It makes sense because: regardless how much fuck we give, we're only fooling ourselves.
 -- We can't and so, wont, give a single fuck.
--- 
+--
 -- It is not law-abiding because:
 --   Law #1: fmap id = id
 --     We notice that applying any transformation, even something like `id`, always results in no fucks given.
@@ -30,15 +34,17 @@ instance Functor Fucks where
 -- | Provided something that wants to make us give a fuck (raise awareness) in regards to the low amount of fucks given,
 -- we ignore the problem and still do not give a fuck.
 --
--- TODO: Laws
+-- "TODO": Laws
 instance Applicative Fucks where
   pure = mempty
   _ <*> _ = mempty
 
--- | No one really gives a fuck about Monads, so we re-use the (much simpler) Functor and Applicative instances.
--- Nevertheless, notice how `join` forces us to give a fuck about Monads. Fine, monads are cool.
--- 
--- TODO: Laws
+-- | No one really gives a fuck about Monads.
+--
+-- "TODO": Laws
 instance Monad Fucks where
-  return = pure
-  x >>= f = join (fmap f x)
+  _ >>= _ = mempty
+
+giveFucks :: Amount -> Maybe (Fucks f)
+giveFucks 0 = Just mempty
+giveFucks _ = Nothing
